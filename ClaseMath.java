6f7d10b8ce77c09a2a7686cd77739cac3da01e9b
diego-gg <diegoricocastro@hotmail.com>
2022-08-21 00:43:11 -0500

    Ejemplo con metodo pow -- potencias

--- ClaseMath.java
+++ ClaseMath.java
@@ -9,5 +9,14 @@ class ClaseMath {
         int resultado = Math.round(num1); // redonde hacia arriba despues de 0,5 o hacia abajo cuando es menor a 0.49.
 
         System.out.println("El numero " + num1 + " redondeado es " + resultado);
+
+
+        // ejemplo con exponenete --pow
+        
+        double base = 5;
+        double exponente = 3;
+        int resultado2 = (int)Math.pow(base, exponente);
+
+        System.out.println("El resultado de elevar " + base + " a la " + exponente + " nos da un valor de " + resultado2);  
     }
 }
