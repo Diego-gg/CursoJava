08e941083e983fdabce855dda472e67659de758d
diego-gg <diegoricocastro@hotmail.com>
2022-08-21 00:23:28 -0500

    agregue primer archivo

---
+++ ClaseMath.java
@@ -0,0 +1,8 @@
+class ClaseMath {
+    public static void main (String[] args) {
+        int numero = 81;
+        double raiz = Math.sqrt(numero);
+
+        System.out.println("La raiz de " + numero + " es: " + raiz);
+    }
+}
