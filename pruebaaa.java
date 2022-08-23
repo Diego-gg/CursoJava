644ac1c3e28e8cc27493b1ef3dbe82922eade689
diego-gg <diegoricocastro@hotmail.com>
2022-08-22 23:37:11 -0500

    Prueba delejericio 1 de Matematicas discretas

---
+++ pruebaaa.java
@@ -0,0 +1,20 @@
+/* public class pruebaaa {
+    public static void main (String[] args) {
+        int i = 1;
+        while ((i>0 && i<3)|| i==3) {
+            System.out.println("x=x+1");
+            i=i+1;
+        }
+    }
+} */
+
+public class pruebaaa {
+    public static void main (String[] args) {
+        int i=1, j=1;
+        while ((i<2 && j<5)|| i+j==5) {
+            i = i+1;;
+            j=j+2;;
+            System.out.println("x=x+1");
+        }
+    }
+}
